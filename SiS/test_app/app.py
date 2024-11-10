ce813270e7f366d6b0c92b37066346c21a528906
GuillaumeGSicara <guillaume.gonde@sicara.com>
2024-11-10 14:16:17 +0100

    feat(SiS): add working env for CI-CD setup (first test)

--- SiS/test_app/app.py
+++ SiS/test_app/app.py
@@ -1,16 +1,12 @@
-from typing import Dict, List, Optional
-
 import _snowflake
 import json
 import streamlit as st
 from snowflake.snowpark.context import get_active_session
 
-# Contents
-
 DATABASE = "DBT_CORTEX"
 SCHEMA = "DEFINITIONS"
-STAGE = "DBT_APP"
-FILE = "dbt_app.yml"
+STAGE = "SEMANTIC_RPT_TAG_CAMPAIGNS_SENDS"
+FILE = "semantic_rpt_tag_campaigns_sends.yml"
 
 def send_message(prompt: str) -> dict:
     """Calls the REST API and returns the response."""
@@ -28,7 +24,6 @@ def send_message(prompt: str) -> dict:
         ],
         "semantic_model_file": f"@{DATABASE}.{SCHEMA}.{STAGE}/{FILE}",
     }
-
     resp = _snowflake.send_snow_api_request(
         "POST",
         f"/api/v2/cortex/analyst/message",
@@ -38,11 +33,9 @@ def send_message(prompt: str) -> dict:
         {},
         30000,
     )
-
     if resp["status"] < 400:
         return json.loads(resp["content"])
     else:
-        st.session_state.messages.pop()
         raise Exception(
             f"Failed request with status {resp['status']}: {resp}"
         )
@@ -57,24 +50,14 @@ def process_message(prompt: str) -> None:
     with st.chat_message("assistant"):
         with st.spinner("Generating response..."):
             response = send_message(prompt=prompt)
-            request_id = response["request_id"]
             content = response["message"]["content"]
-            st.session_state.messages.append(
-                {**response['message'], "request_id": request_id}
-            )
-            display_content(content=content, request_id=request_id)  # type: ignore[arg-type]
+            display_content(content=content)
+    st.session_state.messages.append({"role": "assistant", "content": content})
 
 
-def display_content(
-    content: List[Dict[str, str]],
-    request_id: Optional[str] = None,
-    message_index: Optional[int] = None,
-) -> None:
+def display_content(content: list, message_index: int = None) -> None:
     """Displays a content item for a message."""
     message_index = message_index or len(st.session_state.messages)
-    if request_id:
-        with st.expander("Request ID", expanded=False):
-            st.markdown(request_id)
     for item in content:
         if item["type"] == "text":
             st.markdown(item["text"])
@@ -84,60 +67,38 @@ def display_content(
                     if st.button(suggestion, key=f"{message_index}_{suggestion_index}"):
                         st.session_state.active_suggestion = suggestion
         elif item["type"] == "sql":
-            display_sql(item["statement"])
-
-
-@st.cache_data
-def display_sql(sql: str) -> None:
-    with st.expander("SQL Query", expanded=False):
-        st.code(sql, language="sql")
-    with st.expander("Results", expanded=True):
-        with st.spinner("Running SQL..."):
-            session = get_active_session()
-            df = session.sql(sql).to_pandas()
-            if len(df.index) > 1:
-                data_tab, line_tab, bar_tab = st.tabs(
-                    ["Data", "Line Chart", "Bar Chart"]
-                )
-                data_tab.dataframe(df)
-                if len(df.columns) > 1:
-                    df = df.set_index(df.columns[0])
-                with line_tab:
-                    st.line_chart(df)
-                with bar_tab:
-                    st.bar_chart(df)
-            else:
-                st.dataframe(df)
-
-
-def show_conversation_history() -> None:
-    for message_index, message in enumerate(st.session_state.messages):
-        chat_role = "assistant" if message["role"] == "analyst" else "user"
-        with st.chat_message(chat_role):
-            display_content(
-                content=message["content"],
-                request_id=message.get("request_id"),
-                message_index=message_index,
-            )
-
-
-def reset() -> None:
-    st.session_state.messages = []
-    st.session_state.suggestions = []
-    st.session_state.active_suggestion = None
+            with st.expander("SQL Query", expanded=False):
+                st.code(item["statement"], language="sql")
+            with st.expander("Results", expanded=True):
+                with st.spinner("Running SQL..."):
+                    session = get_active_session()
+                    df = session.sql(item["statement"]).to_pandas()
+                    if len(df.index) > 1:
+                        data_tab, line_tab, bar_tab = st.tabs(
+                            ["Data", "Line Chart", "Bar Chart"]
+                        )
+                        data_tab.dataframe(df)
+                        if len(df.columns) > 1:
+                            df = df.set_index(df.columns[0])
+                        with line_tab:
+                            st.line_chart(df)
+                        with bar_tab:
+                            st.bar_chart(df)
+                    else:
+                        st.dataframe(df)
 
 
 st.title("Cortex analyst")
 st.markdown(f"Semantic Model: `{FILE}`")
 
 if "messages" not in st.session_state:
-    reset()
+    st.session_state.messages = []
+    st.session_state.suggestions = []
+    st.session_state.active_suggestion = None
 
-with st.sidebar:
-    if st.button("Reset conversation"):
-        reset()
-
-show_conversation_history()
+for message_index, message in enumerate(st.session_state.messages):
+    with st.chat_message(message["role"]):
+        display_content(content=message["content"], message_index=message_index)
 
 if user_input := st.chat_input("What is your question?"):
     process_message(prompt=user_input)
